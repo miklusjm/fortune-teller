fc140938f2e12a0facebd7290e24d91fc9b12696
Joseph Miklus <joseph.miklus@gmail.com>
2016-07-08 18:12:14 -0400

    Initial commit

---
+++ FortuneTeller/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FortuneTeller
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Enter your first name.");
+            string firstName = Console.ReadLine();
+            Console.Clear();
+
+            Console.WriteLine("Enter your last name.");
+            string lastName = Console.ReadLine();
+            Console.Clear();
+
+            Console.WriteLine("Enter your age.");
+            int age = int.Parse(Console.ReadLine());
+            Console.Clear();
+
+            Console.WriteLine("Enter your birth month.");
+            string birthMonth = Console.ReadLine();
+            Console.Clear();
+
+            Console.WriteLine("Enter your favorite ROYGBIV color. (Enter \"Help\" for information.)");
+            string ROYGBIV = Console.ReadLine();
+            Console.Clear();
+
+            Console.WriteLine("Enter how many siblings you have.");
+            int siblings = int.Parse(Console.ReadLine());
+            Console.Clear();
+
+            int retireIn;
+            if (age % 2 == 0)
+            {
+                retireIn = 30;
+            }
+            else
+            {
+                retireIn = 50;
+            }
+
+            string location;
+            if (siblings == 0)
+            {
+                location = "Melbourne, Australia";
+            }
+            else if (siblings == 1)
+            {
+                location = "Tokyo, Japan";
+            }
+            else if (siblings == 2)
+            {
+                location = "New York, New York";
+            }
+            else
+            {
+                location = "Paris, France";
+            }
+
+
+
+        }
+    }
+}
